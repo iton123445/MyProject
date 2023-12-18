ff721ccd348c209723fd21b55537f40d5a086c35
iton123445 <paquejefferson@gmail.com>
2023-12-19 02:24:14 +0800

    new update

---
+++ home.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<style>
+
+body {
+  margin: 0; /* Reset default body margin */
+  
+}
+
+ul {
+  list-style-type: none;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  /* background-color: #333; */
+  background-image: linear-gradient(
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0)
+    ),
+    linear-gradient(101deg, #7676f2, #00d4ff);
+    position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #6285f4;
+}
+
+p.bg {
+   font-size: 181px;
+   line-height: 130px;
+   font-weight: 600;
+   text-transform: uppercase;
+   background: url(1745072.gif) center 60%/ cover no-repeat;
+   -webkit-background-clip: text;
+   -webkit-text-fill-color: transparent;
+   font-family: "Lucida Handwriting";
+   
+
+
+}
+.main {
+  padding: 16px;
+  margin-top: 30px;
+  height: 1500px; /* Used in this example to enable scrolling */
+}
+.about {
+  border: 1px solid gray;
+  padding: 8px;
+}
+.about p {
+  text-indent: 50px;
+  text-align: justify;
+  letter-spacing: 3px;
+}
+.about h1 {
+  text-align: center;
+  text-transform: uppercase;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  -webkit-text-fill-color: transparent;
+  background-image: linear-gradient(101deg, #7676f2, #00d4ff);
+}
+.container {
+  border: 2px solid #ccc;
+  background-color: #eee;
+  border-radius: 5px;
+  padding: 16px;
+  margin: 16px 0
+}
+
+.container::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.container img {
+  float: left;
+  margin-right: 20px;
+  border-radius: 25%;
+}
+
+.container span {
+  font-size: 20px;
+  margin-right: 15px;
+}
+
+            
+.shape {
+  width: 100px;
+  height: 100px;
+  background: url(https://scontent.fceb1-1.fna.fbcdn.net/v/t1.6435-9/105388611_1638865089610172_732090850237885786_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeF1WdDmYzO31xS8pwlSLwXPoM_X6NtPL22gz9fo208vbU668gSsvRZHqpvZeIwceiMmRFc2TIWVfAYVR6XCz4Ow&_nc_ohc=Fa0eE_d4ixQAX9Zhf0z&_nc_ht=scontent.fceb1-1.fna&oh=00_AfBZpyZuziF3TQLUPB89XkHgfx2PIyahh0dbBd8M3Bj6PA&oe=65A7E65E)60% center;
+  border-radius: 50%;
+  border-top-right-radius: 0;
+  transform: rotate(-45deg);
+  float: left;
+  margin-top: 50px;
+  margin-left: 20px;
+
+  background-size: cover; /* Add this line to make the background image cover the entire shape */
+  margin-right: 20px;
+  border: 5px solid #33abfa;
+  
+}
+
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #33abfa;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #6285f4;
+}
+
+.containers {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+.header1 {
+    text-align: center;
+  text-transform: uppercase;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  -webkit-text-fill-color: transparent;
+  background-image: linear-gradient(101deg, #7676f2, #00d4ff);
+}
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin: 0;
+  padding: 10px; /* add padding for spacing */
+  overflow: hidden;
+  background-image: linear-gradient(
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0)
+    ),
+    linear-gradient(101deg, #7676f2, #00d4ff);
+}
+
+.footer-content {
+  text-align: center;
+  flex-grow: 1; /* to make it take up remaining space */
+}
+
+.footer-text {
+  margin: 0; /* remove default margin */
+}
+
+.social-icons {
+  display: flex;
+}
+
+.fa {
+  padding: 10px;
+  font-size: 13px;
+  width: 13px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+  opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+
+
+</style>
+<body>
+    <div class="main">
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+<div class="firstimg">
+
+    <center>
+<p class="bg">Best <br>Aqua <br>Delivery <br>in Iligan City</p>
+</center>
+</div>
+
+<div class="about">
+<h1>About us</h1>
+<p><strong>Welcome to Best Aqua Delivery</strong> your trusted source for pure and refreshing water right at your doorstep in Iligan City. At Best Aqua, we understand the importance of staying hydrated with clean and high-quality water, and we are dedicated to making this essential resource easily accessible to you. <br><br>
+<strong> Our Commitment to Purity</strong> We take pride in delivering water that goes through rigorous purification processes, ensuring that every drop is free from impurities and contaminants. Our commitment to quality is unwavering, as we believe that your well-being starts with the water you consume. <br><br>
+
+<strong>Convenience Redefined</strong> Say goodbye to the hassle of lugging heavy water bottles from the store. Best Aqua Delivery brings convenience to your life by delivering crisp and pure water directly to your home or office. Our efficient and reliable delivery service is designed to meet your hydration needs seamlessly. <br><br>
+
+<strong>Local Roots, Global Standards </strong> Born in Iligan City, we are deeply rooted in our local community. Our water is sourced responsibly, and our operations adhere to the highest global standards of hygiene and safety. We are not just a delivery service; we are a part of the community, and we take pride in contributing to the health and well-being of Iligan City residents. <br><br>
+
+<strong>Customer-Centric Approach </strong> At Best Aqua, customer satisfaction is our priority. We strive to exceed your expectations with every delivery. Our friendly and professional team is here to ensure a seamless experience, from placing your order to enjoying a glass of our premium water. <br><br>
+
+<strong>Environmental Responsibility</strong> Beyond delivering quality water, we are committed to environmental responsibility. Our packaging is designed with sustainability in mind, and we actively seek ways to minimize our ecological footprint. Join us in our mission to hydrate responsibly. <br><br>
+
+Choose Best Aqua Delivery for a hydration experience that combines quality, convenience, and community. Trust us to keep your glasses full and your well-being a top priority.</p>
+</div>
+<h1 class="header1">testimonial</h1>
+<div class="container">
+  <div class="shape"></div>
+  <p><span>Gen</span><i> Rating: 5/5</i></p>
+  <p><blockquote>I've been using Best Aqua Delivery for a while now, and I couldn't be happier with their service! The water is always delivered on time, and the quality is exceptional. The convenience of having fresh aqua at my doorstep has made my life so much easier. The delivery staff is friendly and professional. I highly recommend Best Aqua Delivery to anyone looking for reliable and top-notch water delivery service!</blockquote></p>
+</div>
+
+<div class="container">
+<div class="shape"></div>
+  <p><span>Rebecca Flex.</span><i> Rating: 4/5</i></p>
+  <p>I recently tried Best Aqua Delivery, and overall, it's been a positive experience. The water quality is excellent, and the delivery was prompt. The pricing is reasonable too. My only suggestion would be to enhance the communication process. A tracking system or delivery updates would be a great addition. Nonetheless, I'm happy with the service and will continue using it!</p>
+</div>
+
+<div class="container">
+<div class="shape"></div>
+  <p><span>Rebecca Flex.</span><i> Rating: 4.5/5</i></p>
+  <p>I stumbled upon Best Aqua Delivery a few weeks ago, and it's been a game-changer. The water quality is fantastic, and the delivery is consistently on time. What impressed me the most is the eco-friendly packaging. It's clear that they care about the environment. The website is user-friendly, but I'd love to see more variety in package options. Keep up the great work!</p>
+</div>
+<h1 class="header1">Contact Us</h1>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d505549.7735997128!2d124.414563!3d8.141368!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x325579b328c9540d%3A0xe6e208aba2f0d03b!2sIligan%20City%2C%20Lanao%20del%20Norte%2C%20Philippines!5e0!3m2!1sen!2sus!4v1702921690971!5m2!1sen!2sus" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+<div class="containers">
+  <form action="/action_page.php">
+    <label for="fname">Your Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Email</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+  </form>
+</div>
+            
+<div class="footer">
+  <div class="footer-content">
+    <p class="footer-text">Footer</p>
+  </div>
+  <div class="social-icons">
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-instagram"></a>
+  </div>
+</div>
+
+
+</div>
+
+</body>
+</html>
