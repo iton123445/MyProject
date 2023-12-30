be58271723852919f09032df60ba296eeecd2d65
iton123445 <paquejefferson@gmail.com>
2023-12-30 20:46:36 +0800

    update

--- refill.php
+++ refill.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="smiling-water-drop-11549857218bovtski57z-removebg-preview.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Refill Now!</title>
+    <title>Iton's Website</title>
 </head>
 <style>* {
   box-sizing: border-box;
@@ -38,6 +38,7 @@ body {
 @media (max-width: 600px) {
   .column {
     width: 100%;
+    
   }
 }
 </style>
@@ -45,13 +46,15 @@ body {
 <h2>Pick Your Gallon Size</h2>
 
   <div class="row">
-    <div class="column" style="background-color:#eee;">
-      <h2>Column 1</h2>
-      <p>Some text..</p>
+    <div class="column" style="background-color:#eee;" align="center">
+      <h2>Bottle 500ML</h2>
+      <p><img src="b500ml.png" alt="bottle" height='150px' ></p>
+      <p>Refill me now!</p>
     </div>
-    <div class="column" style="background-color:#eee;">
-      <h2>Column 2</h2>
-      <p>Some text..</p>
+    <div class="column" style="background-color:#eee;" align="center">
+    <h2>Bottle 1L</h2>
+      <p><img src="b1l.png" alt="bottle" height='150px' ></p>
+      <p>Refill me now!</p>
     </div>
   </div>
   
